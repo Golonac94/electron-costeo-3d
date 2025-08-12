104bd457256469b38c657e4328ade3e8c97b75ae
Golonac94 <152063773+Golonac94@users.noreply.github.com>
2025-08-12 15:02:39 -0500

    Delete dist directory

--- dist/renderer/lib/money.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.money = void 0;
-const money = (n, currency = "PEN") => n == null ? "-" : new Intl.NumberFormat("es-PE", { style: "currency", currency }).format(n);
-exports.money = money;
